90d6e3640afcf3b561cded846238ad4162c158da
Sebastian Hultstrand <sebastian@storm-net.se>
2020-11-14 22:51:34 +0100

    Updates to the development env * Fixes syntax errors in the migration files * Adds a development docker compose * Updates the readme with some new info on migrations and about the development compose file

--- README.md
+++ README.md
@@ -4,13 +4,35 @@ help me keep track of when and how to take care of my plants.
 
 ## Development
 
+### Local dependencies
+When we are developing, we will sometimes want to have a local version of the
+dependencies for the project for example when testing our migrations and that
+kind of stuff. 
+
+For that purpose we have a docker compose with prepared deployments.
+```bash
+docker-compose -f development-docker-compose.yaml up -d
+```
+
 ### Migrations
 We use the migrations lib [golang-migrate/migrate](https://github.com/golang-migrate/migrate)
 
 #### Create new ones
-Naming style YYYYMMDDHHII_short_description.[up|down].sql
+Naming style YYYYMMDDHHIISS_short_description.[up|down].sql
 
-The content of themigrations should be written as just postgres flavour SQL. 
+The content of the migrations should be written as just postgres flavour SQL.
+
+Example for creating a new set of migration files:
+```bash
+docker run -v $(pwd)/migrations:/migrations migrate/migrate create -ext .sql -dir /migrations name_of_the_migration
+```
+**Don't forget to fix the permissions on the new migrations files**
 
 #### Applying migrations
 To find out how to apply the migrations, have a look at this [tutorial](https://github.com/golang-migrate/migrate#cli-usage)
+
+Example for development use (Provided you are using the development docker
+compose)
+```bash
+docker run -v $(pwd)/migrations:/migrations --network treat-your-plant_dbn migrate/migrate -path=/migrations/ -database postgresql://postgres:postgres@db:5432/treat-your-plant?sslmode=disable up
+```
--- migrations/20201113222200_init.up.sql
+++ migrations/20201113222200_init.up.sql
@@ -1,7 +1,15 @@
+CREATE TABLE plant_types(
+    id serial NOT NULL CONSTRAINT plant_types_pkey PRIMARY KEY,
+    latin_names json,
+    common_names json,
+    created timestamp with time zone not null,
+    updated timestamp with time zone not null
+);
+
 CREATE TABLE plants(
     id uuid NOT NULL CONSTRAINT events_pkey PRIMARY KEY,
     name text,
-    plant_type_id int NOT NULL CONSTRAINT plant_type_id_fk REFERENCES plant_types,
+    plant_type_id int NOT NULL CONSTRAINT plant_type_id_fk REFERENCES plant_types(id),
     created timestamp with time zone not null,
     updated timestamp with time zone not null
 );
@@ -14,18 +22,10 @@ CREATE TABLE users(
 );
 
 CREATE TABLE user_plants(
-    user_id uuid NOT NULL CONSTRAINT user_plants_user_id_fk REFERENCES user,
-    plant_id uuid NOT NULL CONSTRAINT user_plants_plant_id_fk REFERENCES plants,
+    user_id uuid NOT NULL CONSTRAINT user_plants_user_id_fk REFERENCES users(id),
+    plant_id uuid NOT NULL CONSTRAINT user_plants_plant_id_fk REFERENCES plants(id),
     created timestamp with time zone not null,
     UNIQUE (user_id, plant_id)
-);
-
-CREATE TABLE plant_types(
-    id serial NOT NULL CONSTRAINT plant_types_pkey PRIMARY KEY,
-    latin_names json,
-    common_names json,
-    created timestamp with time zone not null,
-    updated timestamp with time zone not null
 );
 
 CREATE TABLE tips(
@@ -39,15 +39,15 @@ CREATE TABLE tips(
 );
 
 CREATE TABLE plant_type_tips(
-    plant_type_id bigint NOT NULL CONSTRAINT plant_type_tips_plant_type_id_fk REFERENCES user,
-    tip_id uuid NOT NULL CONSTRAINT plant_type_tips_tip_id_fk REFERENCES plants,
+    plant_type_id bigint NOT NULL CONSTRAINT plant_type_tips_plant_type_id_fk REFERENCES plant_types(id),
+    tip_id bigint NOT NULL CONSTRAINT plant_type_tips_tip_id_fk REFERENCES tips(id),
     created timestamp with time zone not null,
     UNIQUE (plant_type_id, tip_id)
 );
 
 CREATE TABLE plant_tips(
-    plant_id uuid NOT NULL CONSTRAINT plant_tips_plant_id_fk REFERENCES user,
-    tip_id uuid NOT NULL CONSTRAINT plant_tips_tip_id_fk REFERENCES plants,
+    plant_id uuid NOT NULL CONSTRAINT plant_tips_plant_id_fk REFERENCES plants(id),
+    tip_id bigint NOT NULL CONSTRAINT plant_tips_tip_id_fk REFERENCES tips(id),
     created timestamp with time zone not null,
     UNIQUE (plant_id, tip_id)
 );
